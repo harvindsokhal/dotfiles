25232b50c2413aaaf3faffc96a82da969ff75046
harvindsokhal <harvindsokhal@gmail.com>
2026-04-22 19:44:59 +0100

    auto-session + change detection

--- nvim/lua/harvi/lazy.lua
+++ nvim/lua/harvi/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("harvi.plugins")
+require("lazy").setup("harvi.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})

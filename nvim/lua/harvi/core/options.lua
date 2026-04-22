956cecd83c5eb24a65ec7198ef951041971f55a9
harvindsokhal <harvindsokhal@gmail.com>
2026-04-22 12:48:56 +0100

    Updated options

--- nvim/lua/harvi/core/options.lua
+++ nvim/lua/harvi/core/options.lua
@@ -24,3 +24,22 @@ opt.background = "dark"
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
+
+
+--clipboard
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -NoProfile -Command Get-Clipboard',
+			["*"] = 'powershell.exe -NoProfile -Command Get-Clipboard',
+		},
+		cache_enabled = 0,
+	}
+
+	vim.opt.clipboard = "unnamedplus"
+end

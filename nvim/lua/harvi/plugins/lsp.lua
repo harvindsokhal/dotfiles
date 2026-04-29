6520adb70d28ad871bdf673a384bf6da06957c3a
harvindsokhal <harvindsokhal@gmail.com>
2026-04-29 15:32:31 +0100

    lsp fix

--- nvim/lua/harvi/plugins/lsp.lua
+++ nvim/lua/harvi/plugins/lsp.lua
@@ -1,182 +1,311 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-      library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
-  },
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 
-  {
-    "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-      -- import mason
-      local mason = require("mason")
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
 
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"ts_ls",
+					"html",
+					"cssls",
+					"tailwindcss",
+					"svelte",
+					"graphql",
+					"emmet_ls",
+					"prismals",
+					"pyright",
+					"gopls",
+					"clangd",
+					"dockerls",
+					"docker_compose_language_service",
+					"yamlls",
+					"bashls",
+					"angularls",
+					"vue_ls",
+				},
+				automatic_enable = false,
+			})
 
-      -- import mason-lspconfig
-      local mason_lspconfig = require("mason-lspconfig")
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- formatters
+					"prettier",
+					"stylua",
+					"black",
+					"isort",
+					"gofumpt",
+					"goimports",
+					"clang-format",
+					"shfmt",
 
-      local mason_tool_installer = require("mason-tool-installer")
+					-- linters
+					"eslint_d",
+					"pylint",
+					"golangci-lint",
+					"shellcheck",
+					"hadolint",
+				},
+			})
+		end,
+	},
 
-      mason.setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"folke/lazydev.nvim",
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			local keymap = vim.keymap
 
-      mason_lspconfig.setup({
-        ensure_installed = {
-          "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
-        },
-      })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					local opts = { buffer = ev.buf, silent = true }
 
-      mason_tool_installer.setup({
-        ensure_installed = {
-          "prettier", -- prettier formatter
-          "stylua", -- lua formatter
-          "isort", -- python formatter
-          "black", -- python formatter
-        },
-      })
-    end,
-  },
+					opts.desc = "Show LSP references"
+					keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
-  {
-    "neovim/nvim-lspconfig",
-  },
+					opts.desc = "Go to declaration"
+					keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-      "folke/lazydev.nvim",
-    },
-    config = function()
-      local lspconfig = require("lspconfig")
+					opts.desc = "Show LSP definitions"
+					keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "rust_analyzer",
-          "ts_ls",
-          "html",
-          "cssls",
-          "tailwindcss",
-          "pyright",
-        },
-        handlers = {
-          function(server_name)
-            lspconfig[server_name].setup({
-              capabilities = capabilities,
-            })
-          end,
+					opts.desc = "Show LSP implementations"
+					keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
-          ["lua_ls"] = function()
-            lspconfig.lua_ls.setup({
-              capabilities = capabilities,
-              settings = {
-                Lua = {
-                  completion = {
-                    callSnippet = "Replace",
-                  },
-                  diagnostics = {
-                    globals = { "vim" },
-                  },
-                  workspace = {
-                    checkThirdParty = false,
-                  },
-                  telemetry = {
-                    enable = false,
-                  },
-                },
-              },
-            })
-          end,
+					opts.desc = "Show LSP type definitions"
+					keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-          ["rust_analyzer"] = function()
-            lspconfig.rust_analyzer.setup({
-              capabilities = capabilities,
-              settings = {
-                ["rust-analyzer"] = {
-                  cargo = {
-                    allFeatures = true,
-                  },
-                  checkOnSave = {
-                    command = "clippy",
-                  },
-                },
-              },
-            })
-          end,
+					opts.desc = "Code actions"
+					keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-          ["ts_ls"] = function()
-            lspconfig.ts_ls.setup({
-              capabilities = capabilities,
-              filetypes = {
-                "javascript",
-                "javascriptreact",
-                "typescript",
-                "typescriptreact",
-              },
-            })
-          end,
+					opts.desc = "Rename symbol"
+					keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-          ["cssls"] = function()
-            lspconfig.cssls.setup({
-              capabilities = capabilities,
-              filetypes = {
-                "css",
-                "scss",
-                "less",
-              },
-            })
-          end,
+					opts.desc = "Buffer diagnostics"
+					keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-          ["tailwindcss"] = function()
-            lspconfig.tailwindcss.setup({
-              capabilities = capabilities,
-              filetypes = {
-                "html",
-                "css",
-                "scss",
-                "javascript",
-                "javascriptreact",
-                "typescript",
-                "typescriptreact",
-              },
-            })
-          end,
+					opts.desc = "Line diagnostics"
+					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-          ["pyright"] = function()
-            lspconfig.pyright.setup({
-              capabilities = capabilities,
-            })
-          end,
-        },
-      })
-    end,
-  },
+					opts.desc = "Previous diagnostic"
+					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+					opts.desc = "Next diagnostic"
+					keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+					opts.desc = "Hover docs"
+					keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+					opts.desc = "Restart LSP"
+					keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)
+				end,
+			})
+
+			local signs = {
+				Error = " ",
+				Warn = " ",
+				Hint = "󰠠 ",
+				Info = " ",
+			}
+
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = "Replace",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
+
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+						},
+						checkOnSave = {
+							command = "clippy",
+						},
+					},
+				},
+			})
+
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+				},
+			})
+
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"css",
+					"scss",
+					"less",
+				},
+			})
+
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"html",
+					"css",
+					"scss",
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+					"svelte",
+					"vue",
+				},
+			})
+
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.graphql.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"graphql",
+					"gql",
+					"svelte",
+					"typescriptreact",
+					"javascriptreact",
+				},
+			})
+
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"html",
+					"css",
+					"scss",
+					"sass",
+					"less",
+					"javascriptreact",
+					"typescriptreact",
+					"svelte",
+					"vue",
+				},
+			})
+
+			lspconfig.prismals.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.docker_compose_language_service.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+				settings = {
+					yaml = {
+						keyOrdering = false,
+						schemas = {
+							kubernetes = {
+								"k8s/**/*.yaml",
+								"k8s/**/*.yml",
+								"kubernetes/**/*.yaml",
+								"kubernetes/**/*.yml",
+								"*.k8s.yaml",
+								"*.k8s.yml",
+							},
+						},
+					},
+				},
+			})
+
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+				filetypes = { "sh", "bash", "zsh" },
+			})
+
+			lspconfig.angularls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.vue_ls.setup({
+				capabilities = capabilities,
+			})
+		end,
+	},
 }

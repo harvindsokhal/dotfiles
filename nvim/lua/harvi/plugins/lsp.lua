ad2b7a88b53346c3561937773a071e8706eda7cb
harvindsokhal <harvindsokhal@gmail.com>
2026-04-29 16:17:12 +0100

    fix lsp

--- nvim/lua/harvi/plugins/lsp.lua
+++ nvim/lua/harvi/plugins/lsp.lua
@@ -50,7 +50,6 @@ return {
 					"angularls",
 					"vue_ls",
 				},
-				automatic_enable = false,
 			})
 
 			require("mason-tool-installer").setup({
@@ -79,60 +78,36 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"folke/lazydev.nvim",
-		},
 		config = function()
-			local lspconfig = require("lspconfig")
 			local keymap = vim.keymap
 
+			-- keymaps on attach
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
 					local opts = { buffer = ev.buf, silent = true }
 
-					opts.desc = "Show LSP references"
 					keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-
-					opts.desc = "Go to declaration"
 					keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-					opts.desc = "Show LSP definitions"
 					keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-
-					opts.desc = "Show LSP implementations"
 					keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-
-					opts.desc = "Show LSP type definitions"
 					keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-					opts.desc = "Code actions"
 					keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-					opts.desc = "Rename symbol"
 					keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-					opts.desc = "Buffer diagnostics"
 					keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-
-					opts.desc = "Line diagnostics"
 					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-					opts.desc = "Previous diagnostic"
 					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-
-					opts.desc = "Next diagnostic"
 					keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
-					opts.desc = "Hover docs"
 					keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-					opts.desc = "Restart LSP"
 					keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)
 				end,
 			})
 
+			-- diagnostic signs
 			local signs = {
 				Error = " ",
 				Warn = " ",
@@ -145,41 +120,33 @@ return {
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 			end
 
-			lspconfig.lua_ls.setup({
+			-- =========================
+			-- LSP CONFIGS (NEW API)
+			-- =========================
+
+			vim.lsp.config("lua_ls", {
 				capabilities = capabilities,
 				settings = {
 					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							checkThirdParty = false,
-						},
-						telemetry = {
-							enable = false,
-						},
+						completion = { callSnippet = "Replace" },
+						diagnostics = { globals = { "vim" } },
+						workspace = { checkThirdParty = false },
+						telemetry = { enable = false },
 					},
 				},
 			})
 
-			lspconfig.rust_analyzer.setup({
+			vim.lsp.config("rust_analyzer", {
 				capabilities = capabilities,
 				settings = {
 					["rust-analyzer"] = {
-						cargo = {
-							allFeatures = true,
-						},
-						checkOnSave = {
-							command = "clippy",
-						},
+						cargo = { allFeatures = true },
+						checkOnSave = { command = "clippy" },
 					},
 				},
 			})
 
-			lspconfig.ts_ls.setup({
+			vim.lsp.config("ts_ls", {
 				capabilities = capabilities,
 				filetypes = {
 					"javascript",
@@ -189,20 +156,12 @@ return {
 				},
 			})
 
-			lspconfig.html.setup({
+			vim.lsp.config("cssls", {
 				capabilities = capabilities,
+				filetypes = { "css", "scss", "less" },
 			})
 
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-				filetypes = {
-					"css",
-					"scss",
-					"less",
-				},
-			})
-
-			lspconfig.tailwindcss.setup({
+			vim.lsp.config("tailwindcss", {
 				capabilities = capabilities,
 				filetypes = {
 					"html",
@@ -217,11 +176,7 @@ return {
 				},
 			})
 
-			lspconfig.svelte.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.graphql.setup({
+			vim.lsp.config("graphql", {
 				capabilities = capabilities,
 				filetypes = {
 					"graphql",
@@ -232,7 +187,7 @@ return {
 				},
 			})
 
-			lspconfig.emmet_ls.setup({
+			vim.lsp.config("emmet_ls", {
 				capabilities = capabilities,
 				filetypes = {
 					"html",
@@ -247,65 +202,50 @@ return {
 				},
 			})
 
-			lspconfig.prismals.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.csharp_ls.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.dockerls.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.docker_compose_language_service.setup({
-				capabilities = capabilities,
-			})
-
-			lspconfig.yamlls.setup({
+			vim.lsp.config("yamlls", {
 				capabilities = capabilities,
 				settings = {
 					yaml = {
 						keyOrdering = false,
-						schemas = {
-							kubernetes = {
-								"k8s/**/*.yaml",
-								"k8s/**/*.yml",
-								"kubernetes/**/*.yaml",
-								"kubernetes/**/*.yml",
-								"*.k8s.yaml",
-								"*.k8s.yml",
-							},
-						},
 					},
 				},
 			})
 
-			lspconfig.bashls.setup({
+			vim.lsp.config("bashls", {
 				capabilities = capabilities,
 				filetypes = { "sh", "bash", "zsh" },
 			})
 
-			lspconfig.angularls.setup({
-				capabilities = capabilities,
-			})
+			-- =========================
+			-- ENABLE SERVERS
+			-- =========================
 
-			lspconfig.vue_ls.setup({
-				capabilities = capabilities,
-			})
+			local servers = {
+				"lua_ls",
+				"rust_analyzer",
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"gopls",
+				"clangd",
+				"csharp_ls",
+				"dockerls",
+				"docker_compose_language_service",
+				"yamlls",
+				"bashls",
+				"angularls",
+				"vue_ls",
+			}
+
+			for _, server in ipairs(servers) do
+				vim.lsp.enable(server)
+			end
 		end,
 	},
 }
